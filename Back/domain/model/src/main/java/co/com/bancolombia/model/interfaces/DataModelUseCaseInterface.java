c61bb18d64d2705e1b0e6c9a0e7feadec92ca279
Carlos Arturo Buitrago Zamudio <cbuitragoz@unal.edu.co>
2025-01-19 17:40:16 -0500

    Nueva interfaz para la conexion con el Usecase

---
+++ Back/domain/model/src/main/java/co/com/bancolombia/model/interfaces/DataModelUseCaseInterface.java
@@ -0,0 +1,7 @@
+package co.com.bancolombia.model.interfaces;
+
+import co.com.bancolombia.model.datamodeluser.DataModelUser;
+
+public interface DataModelUseCaseInterface {
+    public DataModelUser GetInfoByUser(int id) ;
+}

817c533acce4fecd784c4b478a48f0cf059c3515
Carlos Arturo Buitrago Zamudio <cbuitragoz@unal.edu.co>
2025-02-10 10:08:56 -0500

    Se refactoriza el back para tener un modelo de respuesta general.
    Se modifica el flujo de trabajo en el front

--- Back/domain/model/src/main/java/co/com/bancolombia/model/responses/ResponseAuthentication.java
+++ Back/domain/model/src/main/java/co/com/bancolombia/model/responses/ResponseAuthentication.java
@@ -1,17 +1,4 @@
 package co.com.bancolombia.model.responses;
 
-
-import co.com.bancolombia.model.datamodeluser.DataModelUser;
-import lombok.*;
-
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder(toBuilder = true)
 public class ResponseAuthentication {
-    private boolean isAuthenticated;
-    private String message;
-    private DataModelUser userInfo;
 }
